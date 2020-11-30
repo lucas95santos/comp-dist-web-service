630f89d6394d85ce25d2888a63f9e0d5c70fe82e
Lucas Santos <lucasa95santos@gmail.com>
2020-11-29 22:03:41 -0400

    chore: adicionando a lib dotenv ao projeto

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+require('dotenv/config');
 const express = require('express');
 
 const server = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 server.use(express.json());
 
