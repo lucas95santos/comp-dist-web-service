2a55bf11e41ef0624ea0cd616f98e2367850567b
Lucas Santos <lucasa95santos@gmail.com>
2020-12-09 19:31:38 -0400

    feat: implementando retorno em xml e json na listagem de atores por filme

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -18,15 +18,53 @@ class MovieController {
 
   async show(request, response) {
     const { id } = request.params;
-    const xmlBuilder = new xml.Builder();
-
     const [realId, type] = String(id).split('.');
+
+    const xmlBuilder = new xml.Builder();
 
     const responseFormat = type && type === 'xml' ? 1 : !type || type === 'json' ? 0 : null;
 
     try {
       const movieService = new MovieService();
       const data = await movieService.select(realId);
+
+      switch (responseFormat) {
+        case 1:
+          response.set('Content-Type', 'text/xml');
+          return response.status(200).send(xmlBuilder.buildObject({
+            ...data,
+            created_at: String(data.created_at),
+            updated_at: String(data.updated_at)
+          }));
+        case 0:
+          return response.status(200).json(data);
+      }
+    } catch (err) {
+      switch (responseFormat) {
+        case 1:
+          response.set('Content-Type', 'text/xml');
+          return response.status(400).send(xmlBuilder.buildObject({
+            error: err.message
+          }));
+        case 0:
+          return response.status(400).json({
+            error: err.message
+          });
+      }
+    }
+  }
+
+  async showActors(request, response) {
+    const { id } = request.params;
+    const [realId, type] = String(id).split('.');
+
+    const xmlBuilder = new xml.Builder();
+
+    const responseFormat = type && type === 'xml' ? 1 : !type || type === 'json' ? 0 : null;
+
+    try {
+      const movieService = new MovieService();
+      const data = await movieService.selectActors(realId);
 
       switch (responseFormat) {
         case 1:
@@ -47,21 +85,6 @@ class MovieController {
             error: err.message
           });
       }
-    }
-  }
-
-  async showActors(request, response) {
-    try {
-      const { id } = request.params;
-
-      const movieService = new MovieService();
-      const data = await movieService.selectActors(id);
-
-      return response.status(200).json(data);
-    } catch (err) {
-      return response.status(400).json({
-        error: err.message
-      });
     }
   }
 
